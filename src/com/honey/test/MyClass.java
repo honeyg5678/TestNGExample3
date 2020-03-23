d1510ddb30c52213e4c65474d0cc3cad842d4a68
swaya <swaya@LAPTOP-AQDKG207.home>
2020-03-22 21:22:50 -0500

    mynew

--- src/com/honey/test/MyClass.java
+++ src/com/honey/test/MyClass.java
@@ -5,6 +5,9 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 12;
+		int b = 90;
+		int c = a + b;
+		System.out.println(c);
 
 	}
 
